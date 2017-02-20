f100d55240b5096305c922aa1a8614ec649eca72
guoxu1231 <guoxu1231@gmail.com>
2017-02-20 23:29:53 +0800

    /ethereum/go-ethereum/accounts

---
+++ dominus/intg/ethereum/account_test.go
@@ -0,0 +1,64 @@
+package ethereum
+
+import (
+	"testing"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/stretchr/testify/assert"
+	"fmt"
+	"os"
+)
+
+const (
+	KEYSTORE_DIR = "/tmp/keystore"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("TestMain Setup")
+	os.RemoveAll(KEYSTORE_DIR)
+	os.Exit(m.Run())
+}
+
+//EE:key or account management
+func TestCreateAccount(t *testing.T) {
+	privkey, _ := crypto.GenerateKey()
+	address := crypto.PubkeyToAddress(privkey.PublicKey)
+
+	var result struct {
+		Privkey string `json:"privkey"`
+		Address string `json:"address"`
+	}
+
+	result.Privkey = common.Bytes2Hex(crypto.FromECDSA(privkey))
+	result.Address = address.Hex()
+	print(address.Hex())
+}
+
+//EE: Fully client side account management without any backing Ethereum node
+func TestGethAccuntManager(t *testing.T) {
+	am := accounts.NewManager(KEYSTORE_DIR, accounts.StandardScryptN, accounts.StandardScryptP)
+	passphrase := "welcome1"
+	newPassphrase := "welcome2"
+
+	//create new account with passphrase
+	newAcc, _ := am.NewAccount(passphrase)
+	fmt.Println("New Account:", newAcc.Address.Hex())
+	assert.Equal(t, 1, len(am.Accounts()))
+
+	//exports account as a JSON key, encrypted with newPassphrase.
+	jsonAcc, err := am.Export(newAcc, "password", newPassphrase);
+	assert.NotNil(t, err)
+	println(err.Error())
+	jsonAcc, err = am.Export(newAcc, passphrase, newPassphrase);
+	fmt.Println("Export decrypted keyJSON:", string(jsonAcc))
+
+	//deletes the key matched by account if the passphrase is correct.
+	_ = am.DeleteAccount(newAcc, passphrase)
+	assert.Equal(t, 0, len(am.Accounts()))
+
+	//stores the given encrypted JSON key into the key directory.
+	impAcc, _ := am.Import(jsonAcc, newPassphrase, passphrase)
+	fmt.Println("Import decrypted keyJSON:", impAcc.Address.Hex())
+	assert.Equal(t, 1, len(am.Accounts()))
+}
