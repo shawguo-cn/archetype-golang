a29630d66bb1e067b029808a5239e3d4454e8100
guoxu1231 <guoxu1231@gmail.com>
2017-03-20 10:33:56 +0800

    dht-spider

---
+++ dominus/intg/dht-spider/getpeers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/shiyanhui/dht"
+	"time"
+)
+
+func main() {
+	d := dht.New(nil)
+	go d.Run()
+
+	for {
+		// ubuntu-14.04.2-desktop-amd64.iso
+		peers, err := d.GetPeers("546cf15f724d19c4319cc17b179d7e035f89c1f4")
+		if err != nil {
+			time.Sleep(time.Second * 1)
+			continue
+		}
+		fmt.Println("Found Peers")
+		for _, peer := range peers {
+			fmt.Printf("\t\t%v\n", peer.IP)
+		}
+
+	}
+}
